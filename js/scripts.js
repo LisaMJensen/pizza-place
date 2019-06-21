d98e4f98adfbf108e0ec7831b64d3e8401c28e89
Lisa Jensen <leese_38@yahoo.com>
2019-06-21 12:22:28 -0700

    remove old priceCalc funtion and add new prototype function to meet the prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,84 @@
 // Business Logic for pizzaOrders
-function PizzaOrders () {
-  this.pizzas = [],
-  this.pizzaId = 0
-}
 
-PizzaOrders.prototype.Pizza = function(pizza) {
-  pizza.id = this.assignId();
-  this.pizzas.push(ticket);
-}
+// function PizzaOrders () {
+//   this.pizzas = [],
+//   this.pizzaId = 0
+// }
+//
+// PizzaOrders.prototype.addPizza = function(pizza) {
+//   pizza.id = this.assignId();
+//   this.pizzas.push(pizza);
+// }
+//
+// PizzaOrders.prototype.assignId = function() {
+//   this.pizzaId += 1;
+//   return this.pizzaId;
+// }
 
-PizzaOrders.prototype.assignId = function() {
-  this.pizzaId += 1;
-  return this.pizzaId;
-}
+// PizzaOrders.prototype.findPizza = function(id) {
+//   for (var i=0; i< this.pizzas.length; i++) {
+//     if (this.pizzas[i]) {
+//       if (this.pizzas[i].id == id) {
+//         return this.pizzas[i];
+//       }
+//     }
+//   };
+//   return false;
+// }
 
-PizzaOrders.prototype.findPizza = function(id) {
-  for (var i=0; i< this.pizzas.length; i++) {
-    if (this.pizzas[i]) {
-      if (this.pizzas[i].id == id) {
-        return this.pizzas[i];
-      }
-    }
-  };
-  return false;
-}
+// PizzaOrders.prototype.deletePizza = function(id) {
+//   for (var i=0; i<this.pizzas.length; i++) {
+//     if (this.pizzas[i]) {
+//       if (this.pizzas[i].id == id) {
+//         delete this.pizzas[i];
+//         return true;
+//       }
+//     }
+//   };
+//   return false;
+// }
 
-PizzaOrders.prototype.deletePizza = function(id) {
-  for (var i=0; i<this.pizzas.length; i++) {
-    if (this.pizzas[i]) {
-      if (this.pizzas[i].id == id) {
-        delete this.pizzas[i];
-        return true;
-      }
-    }
-  };
-  return false;
-}
+// var pizzaIndex = new PizzaOrders();
 
-var pizzaIndex = new PizzaOrders();
 function Pizzas (size, toppings, time, price) {
   this.size = size,
-  this.toppings = toppings;
+  this.toppings = toppings,
   this.time = time,
-  this.price = price
+  this.price = 0
 }
 
-function calcPrice(size, toppings, time) {
-  var pizzaPrice = 7;
-  if (size.includes("medium")) {
-    pizzaPrice += 2;
-  } if (size.includes("large")) {
-    pizzaPrice += 4;
-  } if (size.includes("jumbo")) {
-    pizzaPrice +=6;
-  }
+Pizzas.prototype.calcPrice = function() {
+  // var pizzaPrice = 7;
 
-  // if(toppings.includes("Sausage")) {
-  //   pizzaPrice += 2;
-  // }
-  if(time.includes("9PM" || "10PM")) {
-    pizzaPrice += 2;
+  //calculate size portion of price
+  if (this.size.includes("small")) {
+    this.price += 6
+  } if (this.size.includes("medium")) {
+    this.price += 8
+  } if (this.size.includes("large")) {
+    this.price += 10
+  } if (this.size.includes("jumbo")) {
+    this.price += 12
   }
-  return pizzaPrice;
+  //calculate toppings portion of price
+  if(toppings.length <= 2) {
+    this.price += 0;
+  } if (toppings.length >= 3) {
+    this.price += 2;
+  } if (toppings.length >= 4) {
+    this.price += 3;
+  } if (toppings.length >= 5) {
+    this.price += 4;
+  } if (toppings.length >= 6) {
+    this. price += 8
+   return this.price;
+   console.log(this.price);
 }
-
+}
 function addOutput (arrayNum) {
   $("span#size").html(pizzaIndex.pizzas[arrayNum].size);
   $("span#toppings").html(pizzaIndex.pizzas[arrayNum].toppings);
-  $("span#time").html(pizzaIndex.pizzas[arrayNum].time);
+  // $("span#time").html(pizzaIndex.pizzas[arrayNum].time);
   $("span#price").html("$" + pizzaIndex.pizzas[arrayNum].price);
 }
 
@@ -78,13 +89,15 @@ var inputtedSize = $("input:radio[name=size]:checked").val();
 var inputtedToppings = $("input:checkbox[name=toppings]:checked").val();
 var inputtedTime = $("select#time").val();
 var pizzaTotal = calcPrice(inputtedSize, inputtedToppings, inputtedTime);
-// var newPizza = new Pizzas (inputtedSize, inputtedToppings, inputtedTime, pizzaTotal);
-// pizzaIndex.addPizza(newPizza);
+var newPizza = new Pizzas (inputtedSize, inputtedToppings, inputtedTime, pizzaTotal);
+
+newPizza.calcPrice();
+//pizzaIndex.addPizza(newPizza);
 // var prevID = pizzaIndex.pizzaId - 1;
 // addOutput(prevID);
 
 // alert(inputtedSize);
 // alert(inputtedTime);
-alert(inputtedToppings);
+// alert(inputtedToppings);
 });
 });
